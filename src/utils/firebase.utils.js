70d288a9b6ecc8cc06585be2c212dfc53a3cab80
limgw <iamapokemonmaster@gmail.com>
2022-06-24 21:10:31 +0800

    created authentication page component alongside sign in form

--- src/utils/firebase.utils.js
+++ src/utils/firebase.utils.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app'
-import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword } from 'firebase/auth'
+import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth'
 import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore'
 const firebaseConfig = {
   apiKey: "AIzaSyCT0oNlgemyHhoHDGg5c0o-e309CwEQeYE",
@@ -45,5 +45,11 @@ export const createUserDocumentFromAuth = async(userAuth) => {
 export const createAuthUserWithEmailAndPassword = async(email, password) => {
   if (!email || !password) return;
   return await createUserWithEmailAndPassword(auth, email, password)
+
+}
+
+export const signInAuthUserWithEmailAndPassword = async(email, password) => {
+  if (!email || !password) return;
+  return await signInWithEmailAndPassword(auth, email, password)
 
 }
